ab2f1487dd23eb13994d89c2d10045ad7cb7bafd
royyan <mproyyan@gmail.com>
2022-08-03 09:53:30 +0700

    create product repository and implement all contracts

---
+++ repository/product_repository.go
@@ -0,0 +1,72 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/mproyyan/go-simple-restful/helper"
+	"github.com/mproyyan/go-simple-restful/model"
+)
+
+type ProductRepository struct {
+}
+
+func (pr *ProductRepository) Save(ctx context.Context, tx *sql.Tx, product model.Product) model.Product {
+	script := "INSERT INTO products(name) VALUES(?)"
+	result, err := tx.ExecContext(ctx, script, product.Name)
+	helper.CheckErr(err)
+
+	id, _ := result.LastInsertId()
+	product.Id = int(id)
+
+	return product
+}
+
+func (pr *ProductRepository) Update(ctx context.Context, tx *sql.Tx, product model.Product) model.Product {
+	script := "UPDATE products SET name = ? WHERE id = ?"
+	_, err := tx.ExecContext(ctx, script, product.Name, product.Id)
+	helper.CheckErr(err)
+
+	return product
+}
+
+func (pr *ProductRepository) Delete(ctx context.Context, tx *sql.Tx, product model.Product) {
+	script := "DELETE FROM products WHERE id = ?"
+	_, err := tx.ExecContext(ctx, script, product.Id)
+	helper.CheckErr(err)
+}
+
+func (pr *ProductRepository) Find(ctx context.Context, tx *sql.Tx, productId int) (model.Product, error) {
+	script := "SELECT id, name FROM products WHERE id = ? LIMIT 1"
+	rows, err := tx.QueryContext(ctx, script, productId)
+	helper.CheckErr(err)
+	defer rows.Close()
+
+	product := model.Product{}
+	if rows.Next() {
+		scanErr := rows.Scan(&product.Id, &product.Name)
+		helper.CheckErr(scanErr)
+		return product, nil
+	} else {
+		return product, errors.New("product not found")
+	}
+}
+
+func (pr *ProductRepository) FindAll(ctx context.Context, tx *sql.Tx) []model.Product {
+	script := "SELECT id, name FROM products"
+	rows, err := tx.QueryContext(ctx, script)
+	helper.CheckErr(err)
+	defer rows.Close()
+
+	var products []model.Product
+	for rows.Next() {
+		product := model.Product{}
+		err := rows.Scan(&product.Id, &product.Name)
+		helper.CheckErr(err)
+
+		products = append(products, product)
+	}
+
+	return products
+}
