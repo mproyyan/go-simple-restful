e74a47aa4acff8b8e46a3c3b8344c5dd4bb66431
royyan <mproyyan@gmail.com>
2022-08-03 16:07:29 +0700

    setup database connection

---
+++ app/database.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/mproyyan/go-simple-restful/helper"
+)
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:sussyballs@tcp(localhost:3306)/go-simple-restful")
+	helper.CheckErr(err)
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+}
