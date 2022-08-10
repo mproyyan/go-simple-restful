6d16877d25f1fedb7193da3e36e904a0d786ccd4
royyan <mproyyan@gmail.com>
2022-08-10 09:01:05 +0700

    product repository test with mockery

--- test/product_repository_test.go
+++ test/product_repository_test.go
@@ -5,80 +5,22 @@ import (
 	"database/sql"
 	"testing"
 
+	mks "github.com/mproyyan/go-simple-restful/mocks"
 	"github.com/mproyyan/go-simple-restful/model"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-type ProductRepoMock struct {
-	mock.Mock
-}
-
-var productRepoMock = ProductRepoMock{mock.Mock{}}
-
-func (pr *ProductRepoMock) Save(ctx context.Context, tx *sql.Tx, product model.Product) model.Product {
-	arguments := pr.Called(ctx, tx, product)
-
-	if arguments.Get(0) == nil {
-		panic("no return value")
-	}
-
-	data := arguments.Get(0).(model.Product)
-	return data
-}
-
-func (pr *ProductRepoMock) Update(ctx context.Context, tx *sql.Tx, product model.Product) model.Product {
-	arguments := pr.Called(ctx, tx, product)
-
-	if arguments.Get(0) == nil {
-		panic("no return value")
-	}
-
-	data := arguments.Get(0).(model.Product)
-	return data
-}
-
-func (pr *ProductRepoMock) Delete(ctx context.Context, tx *sql.Tx, product model.Product) bool {
-	arguments := pr.Called(ctx, tx, product)
-
-	if arguments.Get(0) == nil {
-		panic("no return value")
-	}
-
-	data := arguments.Get(0).(bool)
-	return data
-}
-
-func (pr *ProductRepoMock) Find(ctx context.Context, tx *sql.Tx, productId int) (model.Product, error) {
-	arguments := pr.Called(ctx, tx, productId)
-
-	if arguments.Get(0) == nil {
-		panic("no return value")
-	}
-
-	data := arguments.Get(0).(model.Product)
-	return data, nil
-}
-
-func (pr *ProductRepoMock) FindAll(ctx context.Context, tx *sql.Tx) []model.Product {
-	arguments := pr.Called(ctx, tx)
-
-	if arguments.Get(0) == nil {
-		panic("no return value")
-	}
-
-	data := arguments.Get(0).([]model.Product)
-	return data
-}
-
 func TestRepoSaveSuccess(t *testing.T) {
 	expectedProduct := model.Product{Id: 1, Name: "Test"}
 
-	productRepoMock.
-		On("Save", context.Background(), new(sql.Tx), model.Product{Name: "Test"}).
-		Return(expectedProduct)
+	mockery := mks.NewProductRepositoryContract(t)
+	mockery.On("Save", mock.Anything, mock.Anything, mock.Anything).
+		Return(func(ctx context.Context, tx *sql.Tx, product model.Product) model.Product {
+			return product
+		})
 
-	product := productRepoMock.Save(context.Background(), new(sql.Tx), model.Product{Name: "Test"})
+	product := mockery.Save(context.Background(), new(sql.Tx), model.Product{Id: 1, Name: "Test"})
 
 	assert.Equal(t, expectedProduct.Id, product.Id)
 	assert.Equal(t, expectedProduct.Name, product.Name)
@@ -87,22 +29,26 @@ func TestRepoSaveSuccess(t *testing.T) {
 func TestRepoUpdateSuccess(t *testing.T) {
 	expectedProduct := model.Product{Id: 1, Name: "Updated"}
 
-	productRepoMock.
-		On("Update", context.Background(), new(sql.Tx), expectedProduct).
-		Return(expectedProduct)
+	mockery := mks.NewProductRepositoryContract(t)
+	mockery.On("Update", mock.Anything, mock.Anything, mock.Anything).
+		Return(func(ctx context.Context, tx *sql.Tx, product model.Product) model.Product {
+			return product
+		})
 
-	product := productRepoMock.Update(context.Background(), new(sql.Tx), model.Product{Id: 1, Name: "Updated"})
+	product := mockery.Update(context.Background(), new(sql.Tx), model.Product{Id: 1, Name: "Updated"})
 
 	assert.Equal(t, expectedProduct.Id, product.Id)
 	assert.Equal(t, expectedProduct.Name, product.Name)
 }
 
 func TestRepoDeleteSuccess(t *testing.T) {
-	productRepoMock.
-		On("Delete", context.Background(), new(sql.Tx), model.Product{Id: 1, Name: "Deleted"}).
-		Return(true)
+	mockery := mks.NewProductRepositoryContract(t)
+	mockery.On("Delete", mock.Anything, mock.Anything, mock.Anything).
+		Return(func(ctx context.Context, tx *sql.Tx, product model.Product) bool {
+			return true
+		})
 
-	result := productRepoMock.Delete(context.Background(), new(sql.Tx), model.Product{Id: 1, Name: "Deleted"})
+	result := mockery.Delete(context.Background(), new(sql.Tx), model.Product{Id: 1, Name: "Deleted"})
 
 	assert.True(t, result)
 }
@@ -110,11 +56,11 @@ func TestRepoDeleteSuccess(t *testing.T) {
 func TestRepoFindSuccess(t *testing.T) {
 	expectedProduct := model.Product{Id: 7, Name: "Find"}
 
-	productRepoMock.
-		On("Find", context.Background(), new(sql.Tx), 7).
-		Return(expectedProduct)
+	mockery := mks.NewProductRepositoryContract(t)
+	mockery.On("Find", mock.Anything, mock.Anything, mock.AnythingOfType("int")).
+		Return(expectedProduct, nil)
 
-	result, err := productRepoMock.Find(context.Background(), new(sql.Tx), 7)
+	result, err := mockery.Find(context.Background(), new(sql.Tx), 7)
 
 	assert.Nil(t, err)
 	assert.Equal(t, expectedProduct.Id, result.Id)
@@ -129,11 +75,11 @@ func TestRepoFindAllSucces(t *testing.T) {
 		},
 	}
 
-	productRepoMock.
-		On("FindAll", context.Background(), new(sql.Tx)).
+	mockery := mks.NewProductRepositoryContract(t)
+	mockery.On("FindAll", mock.Anything, mock.Anything).
 		Return(expectedProduct)
 
-	products := productRepoMock.FindAll(context.Background(), new(sql.Tx))
+	products := mockery.FindAll(context.Background(), new(sql.Tx))
 	product := products[0]
 
 	assert.Equal(t, 7, product.Id)
